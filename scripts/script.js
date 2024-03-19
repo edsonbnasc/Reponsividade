416b495eb1702c1d2bae35a42d101b293b2c797b
Edson Batista <edsonbnasc@uninove.edu.br>
2024-03-19 20:21:02 -0300

    criacao dos botoes e js

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,26 @@ const headerLinks = document.querySelector(".header-style__links");
 
     document.getElementById("headleNavbar").addEventListener("click", ()=>{
         headerLinks.classList.toggle("open")
-})
+});
 const headerUser = document.querySelector(".header-style__user");
     document.getElementById("headleNavuser").addEventListener("click", ()=>{
         headerUser.classList.toggle("user")
-    })
+});
+// função para setar o tema no local storage do navegador
+function setTheme(theme){
+    localStorage.setItem("theme", theme)
+    document.documentElement.className = theme
+}
+
+setTheme()
+
+function getTheme(){
+    if(localStorage.getItem("theme") === "theme-dark"){
+        document.getElementById("toggle").classList.replace("bi-moon-fill", "bi-brightness-high-fill");
+        setTheme("theme-light");
+    }else{
+        document.getElementById("toggle").classList.replace("bi-brightness-high-fill", "bi-moon-fill");
+        setTheme("theme-dark");
+    }
+
+}
